d4a17ac0805a4209c4d361205b8eb1db9779f3f8
Remy DUTHU <remy.duthu@gmail.com>
2022-11-09 09:58:36 +0100

    [iterm] Download Solarized Light

--- modules/iterm/install.zsh
+++ modules/iterm/install.zsh
@@ -8,3 +8,4 @@ source "${DOTFILES_PATH}"/src/*.zsh
 
 taskf "Download themes"
 curl --location https://raw.githubusercontent.com/dracula/iterm/master/Dracula.itermcolors > "${DOTFILES_PATH}/modules/iterm/Dracula.itermcolors"
+curl --location https://raw.githubusercontent.com/altercation/solarized/master/iterm2-colors-solarized/Solarized%20Light.itermcolors > "${DOTFILES_PATH}/modules/iterm/Solarized Light.itermcolors"
