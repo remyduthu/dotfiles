315f1359c9bc0d8ae0c52fc00acd338021f884d6
Remy DUTHU <remy.duthu@gmail.com>
2023-05-31 09:30:33 +0200

    Install Helm autocompletion

--- modules/zsh/install.zsh
+++ modules/zsh/install.zsh
@@ -10,12 +10,13 @@ function as_sudo() {
   sudo bash -c "${@}"
 }
 
-require "kubectl" "zsh"
+require "helm" "kubectl" "kustomize" "zsh"
 
 taskf "Install external completion files"
 FPATH=/usr/local/share/zsh/site-functions/
 as_sudo "mkdir -p ${FPATH}"
 as_sudo "curl --location https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker > ${FPATH}/_docker"
+as_sudo "helm completion zsh > ${FPATH}/_helm"
 as_sudo "kubectl completion zsh > ${FPATH}/_kubectl"
 as_sudo "kustomize completion zsh > ${FPATH}/_kustomize"
 
