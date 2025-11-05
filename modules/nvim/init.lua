e44a176ccc7c0dbb663f5beaa3e582c22d77e8ec
Remy DUTHU <remy.duthu@gmail.com>
2025-11-05 09:03:43 +0100

    [nvim] Add minimal config
    Change-Id: I8822425cef72c40ad42a8a33090584eb10e71d24

--- modules/nvim/init.lua
+++ modules/nvim/init.lua
@@ -1,3 +1,20 @@
-vim.keymap.set("n", "<Space>", "")
+-- Set <space> as the leader key.
+-- See `:help mapleader`.
+-- Must happen before plugins are loaded (otherwise wrong leader will be used).
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- Sync clipboard between OS and Neovim.
+-- Schedule the setting after `UiEnter` because it can increase startup-time.
+-- See `:help clipboard`.
+vim.schedule(function()
+  vim.o.clipboard = "unnamedplus"
+end)
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term.
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Clear highlights on search when pressing <Esc> in normal mode.
+-- See `:help hlsearch`.
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
