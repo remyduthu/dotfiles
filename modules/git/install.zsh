393c6fc907e93dad9eab8011bde93aac6139568d
Remy DUTHU <remy.duthu@gmail.com>
2026-03-10 10:22:50 +0100

    [git] Add custom commands
    Change-Id: Ib602879a635557e29df2144a6585ad494af02ce4

--- modules/git/install.zsh
+++ modules/git/install.zsh
@@ -11,3 +11,8 @@ require "git"
 taskf "Link configuration files"
 link "${DOTFILES_PATH}/modules/git/.gitconfig" "${HOME}/.gitconfig"
 link "${DOTFILES_PATH}/modules/git/.gitignore" "${HOME}/.gitignore"
+
+taskf "Link custom Git commands"
+mkdir -p "${HOME}/.local/bin"
+link "${DOTFILES_PATH}/modules/git/bin/git-cleanup" "${HOME}/.local/bin/git-cleanup"
+link "${DOTFILES_PATH}/modules/git/bin/git-worktree-init" "${HOME}/.local/bin/git-worktree-init"
