621f32b695f3c684328dedd70895f434913fb81b
Remy DUTHU <remy.duthu@gmail.com>
2026-03-13 11:21:42 +0100

    [gh] Add gh-reviews-dashboard script
    Change-Id: If3c5cc4545e5bbc65dbcd16d5964ab1295219789

--- modules/gh/install.zsh
+++ modules/gh/install.zsh
@@ -14,3 +14,12 @@ link "${DOTFILES_PATH}/modules/gh/config.yml" "${HOME}/.config/gh/config.yml"
 taskf "Link scripts"
 link "${DOTFILES_PATH}/modules/gh/bin/gh-daily" "${HOME}/.local/bin/gh-daily"
 link "${DOTFILES_PATH}/modules/gh/bin/gh-reviews" "${HOME}/.local/bin/gh-reviews"
+link "${DOTFILES_PATH}/modules/gh/bin/gh-reviews-dashboard" "${HOME}/.local/bin/gh-reviews-dashboard"
+
+taskf "Install reviews dashboard launchd agent"
+PLIST_SRC="${DOTFILES_PATH}/modules/gh/com.github.reviews-dashboard.plist"
+PLIST_DST="${HOME}/Library/LaunchAgents/com.github.reviews-dashboard.plist"
+mkdir -p "${HOME}/Library/LaunchAgents"
+link "${PLIST_SRC}" "${PLIST_DST}"
+launchctl bootout "gui/$(id -u)" "${PLIST_DST}" 2>/dev/null || true
+launchctl bootstrap "gui/$(id -u)" "${PLIST_DST}"
